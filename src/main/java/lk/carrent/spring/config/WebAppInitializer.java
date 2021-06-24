98e3baa50a18ba3bfc0ad3a0d6a7a69ce6afc1bf
Kavishka Nipun <kavishkanipunofficial@gmail.com>
2021-06-23 20:51:13 -0700

    add some parts for photo upload

--- src/main/java/lk/carrent/spring/config/WebAppInitializer.java
+++ src/main/java/lk/carrent/spring/config/WebAppInitializer.java
@@ -27,16 +27,6 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 
     @Override
     protected void customizeRegistration(ServletRegistration.Dynamic registration) {
-
-        // upload temp file will put here
-        File uploadDirectory = new File(System.getProperty("java.io.tmpdir"));
-
-        // register a MultipartConfigElement
-        MultipartConfigElement multipartConfigElement =
-                new MultipartConfigElement(uploadDirectory.getAbsolutePath(),
-                        maxUploadSizeInMb, maxUploadSizeInMb * 2, maxUploadSizeInMb / 2);
-
-        registration.setMultipartConfig(multipartConfigElement);
-
+        registration.setMultipartConfig(new MultipartConfigElement(System.getProperty("java.io.tmpdir")));
     }
 }
